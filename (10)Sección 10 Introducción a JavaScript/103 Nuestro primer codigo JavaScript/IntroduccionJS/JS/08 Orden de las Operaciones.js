f84cd56f27d76071bddc1f32b87aa06e65679664
Lester Toledo <seeaemprendedor2019@gmail.com>
2026-01-30 15:25:55 -0700

    08 Controlar el Orden de las operaciones mas operador incremento y decremento

---
+++ (10)Sección 10 Introducción a JavaScript/103 Nuestro primer codigo JavaScript/IntroduccionJS/JS/08 Orden de las Operaciones.js
@@ -0,0 +1,32 @@
+
+//Orden de las Operaciones
+
+let resultado;
+
+resultado = (20 + 30 ) * 2;
+
+resultado = (600 + 600 ) * 1.05;
+
+console.log(resultado);
+
+//Incrementos
+
+let puntaje = 10;
+
+puntaje++;  // Muestra el valor y luego lo incrementa
+
+++puntaje; // incrementa el valor y luego lo muestra
+
+//Decrementos
+
+puntaje--; // muestra el valor y luego decrementa
+
+--puntaje;  // decrementa y luego muestra el valor
+
+// Sumar un valor diferente a 1 y asignarlo a la variable
+puntaje += 10;
+
+// Resta un valor diferente a 1 y asignarlo a la variable
+puntaje -= 10;
+
+console.log(puntaje);
