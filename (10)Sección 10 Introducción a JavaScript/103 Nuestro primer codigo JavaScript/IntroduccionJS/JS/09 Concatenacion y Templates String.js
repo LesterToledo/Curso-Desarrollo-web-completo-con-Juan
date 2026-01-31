ff18024f8df4c22342b4164163f28c4c38298417
Lester Toledo <seeaemprendedor2019@gmail.com>
2026-01-30 20:17:20 -0700

    09 Concatenacion y Templates String

---
+++ (10)Sección 10 Introducción a JavaScript/103 Nuestro primer codigo JavaScript/IntroduccionJS/JS/09 Concatenacion y Templates String.js
@@ -0,0 +1,22 @@
+
+//Concatenación y Templates String
+
+const nombre = "Lester";
+
+const correo = "lesterkll79@gmail.com";
+
+console.log(nombre);
+
+console.log(correo);
+
+//Concatenando nombre y correo
+console.log(nombre + correo);
+
+//Concatenando nombre y correo con espacio y con el signo de mas
+console.log("Nombre Cliente:" + nombre + " Correo:" + correo);
+
+//Concatenando nombre y correo con espacio y con el signo de coma
+console.log("Nombre Cliente:", nombre , " Correo:", correo);
+
+//Templates String o String Literals
+console.log(`Nombre Cliente: ${nombre} Correo: ${correo}`);
